30578ca0ea831fb269cbdae4c46f7081170ba5ed
ChenSeanH <chenseanhuang@gmail.com>
2022-10-08 19:18:41 -0400

    data updates

--- Question6.sql
+++ Question6.sql
@@ -14,3 +14,23 @@ INSERT INTO `Authors` VALUES('A03', 'Amy', 'Diane', '1976-04-06', 'Atlanta');
 INSERT INTO `Publishers` VALUES('P00', 'Router', 'Boston', 'MA', 'USA');
 INSERT INTO `Publishers` VALUES('P01', 'Grill', 'Cambridge', 'MA', 'USA');
 INSERT INTO `Publishers` VALUES('P02', 'GreePress', 'London', NULL, 'UK');
+
+INSERT INTO `Editors` VALUES('E01', 'Shawn', 'Brown', '150/hr');
+INSERT INTO `Editors` VALUES('E02', 'Amy', 'Wood', '125/hr');
+INSERT INTO `Editors` VALUES('E03', 'Zackery', 'Hayden', '138/hr');
+
+INSERT INTO `Customers` VALUES('C00', '82 Ames St.', 'Cambridge', 'MA', 'USA');
+INSERT INTO `Customers` VALUES('C01', '94 Brooklyn Ave.', 'Boston', 'MA', 'USA');
+INSERT INTO `Customers` VALUES('C02', '192.168.3.72.', NULL, NULL, NULL);
+INSERT INTO `Customers` VALUES('C03', '18.82.34.176.', NULL, NULL, NULL);
+
+INSERT INTO `Orders` VALUES('O00', '2020-01-02.', 'CH00', 1, 'C00', 'S00', 'B00');
+INSERT INTO `Orders` VALUES('O00', '2020-01-02.', 'CH00', 1, 'C00', 'S00', 'B01');
+INSERT INTO `Orders` VALUES('O00', '2020-01-02.', 'CH00', 1, 'C00', 'S00', 'B02');
+INSERT INTO `Orders` VALUES('O01', '2021-04-22.', 'CH01', 2, 'C01', 'S01', 'B03');
+INSERT INTO `Orders` VALUES('O02', '2022-08-07.', 'CH02', 1, 'C02', 'S02', 'B02');
+INSERT INTO `Orders` VALUES('O03', '2019-11-27.', 'CH01', 2, 'C03', 'S03', 'B01');
+
+INSERT INTO `Channels` VALUES('CH00', 'MITEdu.', '77 Mass. Ave.');
+INSERT INTO `Channels` VALUES('CH01', 'GrayPublish.', '77 Mass. Ave.');
+INSERT INTO `Channels` VALUES('CH02', 'AmazonEbook.', 'Online');
--- Question7.sql
+++ Question7.sql
@@ -30,11 +30,34 @@ FROM Publishers
 INNER JOIN Books
 ON Publishers.PublisherID = Books.PublisherID;
 
-
-
 # 7.7. Editors per book.
+SELECT Books.BookTitle, Editors.EditorID, Editors.FirstName, Editors.LastName
+FROM Books
+INNER JOIN Editors
+ON Books.EditorID = Editors.EditorID;
+
 # 7.8. Books per editor.
+SELECT Editors.FirstName, Editors.LastName, Books.BookID, Books.BookTitle
+FROM Editors
+INNER JOIN Books
+ON Books.EditorID = Editors.EditorID;
+
 # 7.9. Books in an order.
+SELECT Orders.OrderID, Books.BookID, Books.BookTitle
+FROM Orders
+INNER JOIN Books
+ON Books.BookID = Orders.BookID;
+
 # 7.10. Orders for a book.
+SELECT Books.BookID, Books.BookTitle, Orders.OrderID
+FROM Books
+INNER JOIN Orders
+ON Books.BookID = Orders.BookID;
+
 # 7.11. Customer orders.
+SELECT OrderID, Orders.CustomerID
+FROM Orders;
+
 # 7.12. Orders per customer.
+SELECT Orders.CustomerID, OrderID
+FROM Orders;
